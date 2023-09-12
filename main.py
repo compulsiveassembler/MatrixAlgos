511156b0e1785fa9e3d5cd2c89946ef759bc5e2f
C_a <117479548+compulsiveassembler@users.noreply.github.com>
2023-09-12 16:26:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,9 @@ def scale_row(matrix: list[list[float]], row: int, scalar: float):
 
 
 #mutates passed list
-def combine_rows(matrix: list[list[float]], row_1: int, row_2: int):
+def combine_rows(matrix: list[list[float]], row_1: int, row_2: int, scalar: int):
 
+    
 
     return
 
@@ -44,11 +45,7 @@ def combine_rows(matrix: list[list[float]], row_1: int, row_2: int):
 #TODO -> determine if this is optimal formatting for 80 char margins
 def get_scaled_row(matrix: list[list[float]], row: int, scalar: float) \
                                                                 -> list[float]:
-
-
-    return 
-
-
+    return [x*scalar for x in matrix[row]]
 
 
 #entry point for program
@@ -60,6 +57,16 @@ def program_start() -> None:
     print(our_matrix)
     print(our_matrix[0][0])
     print(len(our_matrix[1]))
+
+    print("Pick row to scale")
+    chosen_row: int = int(input())
+    print(f"You will be printing the following row: { our_matrix[chosen_row] }")
+
+    print("Pick a number to scale this row by")
+    scalar: float = float(input())
+
+    print("This is the scaled row V V V")
+    print( get_scaled_row(our_matrix, chosen_row, scalar) )
 
     #solve matrix
 
