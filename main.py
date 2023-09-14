01b0647682aadfd66a5c5f747a96a9b4baf5bc4d
C_a <117479548+compulsiveassembler@users.noreply.github.com>
2023-09-14 03:48:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main() -> None:
     print(our_matrix)
  
     solve_matrix(our_matrix)
-    
+
+ 
+    print(our_matrix)
 
     return
 
@@ -22,28 +24,50 @@ def get_matrix_from_input() -> list[list[float]]:
         
         if (input_string.upper() == "QUIT"):
             break       
-
-        entry.append( [float(x) for x in input_string.split()] ) 
+        else:
+            entry.append( [float(x) for x in input_string.split()] ) 
             
     return entry
 
 def solve_matrix(matrix: list[list[float]]) -> list[list[float]]:
 
-    row_length: int =     len(matrix[0])
+    row_length: int     = len(matrix[0])
     number_of_rows: int = len(matrix)
 
-    pivot_row: int =    0
+    pivot_row: int    = 0
     pivot_column: int = 0
 
-    while(true):
+    target_row: int   = pivot_row + 1
+
+    while(True):
+
+        print(matrix)
+
+        if (pivot_row == number_of_rows or pivot_column == row_length):
+            break
 
         if (matrix[pivot_row][pivot_column] == 0):
             
             for x in range(pivot_row, number_of_rows):
                 if matrix[x][pivot_column] != 0:
-                    matrix = swap_rows(matrix, pivot_row, x)
+                    swap_rows(matrix, pivot_row, x)
             continue
-        
+        else:
+            
+            if (target_row != number_of_rows):
+                scalar: float = (1\
+                                /matrix[pivot_row][pivot_column])\
+                                *-matrix[target_row][pivot_column]
+                print(scalar)
+
+                combine_rows(matrix, pivot_row, target_row, scalar)
+                target_row = target_row + 1
+            else:
+#                pivot_row = pivot_row + 1
+                pivot_column = pivot_column + 1
+                target_row = target_row + 1
+                continue
+
 
 
     return
@@ -51,10 +75,18 @@ def solve_matrix(matrix: list[list[float]]) -> list[list[float]]:
 def swap_rows(
     matrix: list[list[float]], 
     first_row_number: int,
-    second_row_number: int,
-)-> list[list[float]]: 
+    second_row_number: int
+): 
 
-    return
+    row_length: int     = len(matrix[0])
+    number_of_rows: int = len(matrix)
+
+    first_row: list[float] = matrix[first_row_number]
+
+    matrix[first_row_number]  = matrix[second_row_number]
+    matrix[second_row_number] = first_row
+
+    return 
 
 
 def scale_row(
@@ -63,32 +95,26 @@ def scale_row(
     scalar: float,
 )-> list[list[float]]:
 
-    matrix[row] = [x*scalar for x in matrix[row]]
-
-    return
+    return [x*scalar for x in matrix[row]]
 
 def combine_rows(
     matrix: list[list[float]], 
     row_1: int, 
     row_2: int, 
-    scalar: float,
-)-> list[list[float]]:
+    scalar: float
+):
 
-    matrix = scale_row(matrix, row_1, scalar)
+    row_length: int     = len(matrix[0])
+    number_of_rows: int = len(matrix)
+
+    scaled_row = scale_row(matrix, row_1, scalar)
+
+    print(f"scaled row is {scaled_row}")
  
-    for x in range()
+    for x in range(0, row_length):
+        matrix[row_2][x] = matrix[row_2][x] + scaled_row[x]
 
     return
-
-#TODO -> determine if this is optimal formatting for 80 char margins
-def get_scaled_row(
-    matrix: list[list[float]], 
-    row: int, 
-    scalar: float,
-)-> list[float]:
-    
-    return [x*scalar for x in matrix[row]]
-
 
 ################################################################################
 main()
