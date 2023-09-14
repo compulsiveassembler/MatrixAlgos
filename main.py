a7fed39a43fca4d60866ea58f7412de7039122e2
C_a <117479548+compulsiveassembler@users.noreply.github.com>
2023-09-13 19:32:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,8 @@
-def get_matrix_from_input() -> list[list[float]]:
+#ENTRY POINT
 
-    entry: list[list[float]] = []
-
-    loop_trigger: bool = True
-
-    #TODO -> implement a less ugly loop
-    while(True):
-        input_string: str = input()
-        
-        if (input_string.upper() == "QUIT"):
-            break       
-
-        entry.append( [float(x) for x in input_string.split()] )
-     
-    return entry
-
-#///Row Operations
-#///
-
-
-#mutates passed list
-def swap_rows(matrix: list[list[float]], row_number: int): 
-
-
-    return
-
-
-#mutates passed list
-def scale_row(matrix: list[list[float]], row: int, scalar: float):
-
-
-    return
-
-
-#mutates passed list
-def combine_rows(matrix: list[list[float]], row_1: int, row_2: int, scalar: int):
-
-    
-
-    return
-
-
-
-#TODO -> determine if this is optimal formatting for 80 char margins
-def get_scaled_row(matrix: list[list[float]], row: int, scalar: float) \
-                                                                -> list[float]:
-    return [x*scalar for x in matrix[row]]
-
-
-#entry point for program
 #TODO -> restructure to create API for other modules/programs
-def program_start() -> None:
+def main() -> None:
     
-    #get matrix from input
     our_matrix: list[list[float]] = get_matrix_from_input()
     print(our_matrix)
     print(our_matrix[0][0])
@@ -68,9 +18,74 @@ def program_start() -> None:
     print("This is the scaled row V V V")
     print( get_scaled_row(our_matrix, chosen_row, scalar) )
 
-    #solve matrix
+    return
+
+################################################################################
+def get_matrix_from_input() -> list[list[float]]:
+
+    entry: list[list[float]] = []
+
+    #TODO -> implement a less ugly loop
+    while(True):
+        input_string: str = input()
+        
+        if (input_string.upper() == "QUIT"):
+            break       
+
+        entry.append( [float(x) for x in input_string.split()] ) 
+            
+    return entry
+
+#///Row Operations
+#///
+
+
+
+def swap_rows(
+    matrix: list[list[float]], 
+    row_number: int,
+)-> list[list[float]]: 
+
 
     return
 
-program_start()
 
+
+def scale_row(
+    matrix: list[list[float]], 
+    row: int, 
+    scalar: float,
+)-> list[list[float]]:
+
+
+    return
+
+
+
+def combine_rows(
+    matrix: list[list[float]], 
+    row_1: int, 
+    row_2: int, 
+    scalar: float,
+)-> list[list[float]]:
+
+    
+    return
+
+
+
+#TODO -> determine if this is optimal formatting for 80 char margins
+def get_scaled_row(
+    matrix: list[list[float]], 
+    row: int, 
+    scalar: float,
+)-> list[float]:
+    
+    return [x*scalar for x in matrix[row]]
+
+
+################################################################################
+main()
+
+if __name__ == '__main__':
+    main()
